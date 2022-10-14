378cd6062cc25ed7e7fc0e8fd0831c875245ac48
Muhammad Kamran <mkrnaqeebi@gmail.com>
2022-10-14 19:07:13 +0500

    init

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
+import Hero from './components/Hero.vue';
+// import About from './components/About.vue';
+import Statistics from './components/Statistics.vue';
+import Services from './components/Services.vue';
+import Projects from './components/Projects.vue';
+import Contact from './components/Contact.vue';
+import Testimonials from './components/Testimonials.vue';
+// import Cta from './components/Cta.vue';
+
+</script>
+
+<template>
+  <Header />
+  <Hero />
+  <!-- <About /> -->
+  <Statistics />
+  <Services />
+  <Testimonials />
+  <!-- <Cta /> -->
+  <Projects />
+  <Contact />
+  <Footer />
+</template>
