429e634788158186f6e413509d62fb4857ce5c72
aishatasaduq <aishatasaduq@github.com>
2023-03-10 12:08:18 +0000

    removed changes from app file

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
-  <AllOpenIssues v-if="github" />
-  <!-- <GitHub v-if="github" /> -->
+  <GitHub v-if="github" />
   <HomeView v-else />
 </template>
 
@@ -8,25 +7,16 @@
 import { mapGetters } from "vuex";
 
 import HomeView from "./views/HomeView.vue";
-// import GitHub from "./views/GitHub.vue";
-import AllOpenIssues from "./views/AllOpenIssues.vue";
+import GitHub from "./views/GitHub.vue";
 
 export default {
   name: "App",
   components: {
     HomeView,
-    // GitHub,
-    AllOpenIssues
+    GitHub,
   },
   computed: {
     ...mapGetters(["github"]),
   },
 };
 </script>
-<style>
-@font-face {
-  font-family: "Montserrat";
-  src: local("Montserrat"),
-   url(./fonts/montserrat-500.ttf) format("truetype");
-}
-</style>
