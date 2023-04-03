aa5ea36ca2ed1e0f6f1d6b9b4692bd2765facd08
AliHanifdev1 <117160903+AliHanifdev1@users.noreply.github.com>
2023-04-03 14:30:24 +0500

    Create Page as per mock up (#9)
    * new page create
    
    * select remove
    
    * input type changed
    
    * changes done
    
    * fix spelling issue
    
    * changes complete
    
    * fix repositories props type
    
    ---------
    
    Co-authored-by: bob <bob@>
    Co-authored-by: Muhammad Kamran <mkrnaqeebi@gmail.com>

---
+++ src/components/HeaderView.vue
@@ -0,0 +1,77 @@
+<template>
+    <header class="text-gray-600 body-font my-header">
+      <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+        <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+          <img src="../assets/logo.svg" />
+        </a>
+        <nav class="md:ml-11 flex flex-wrap items-center text-base justify-center">
+          <button
+            @click="selectAllOpenIssues"
+            v-bind:class="this.selectAllIssues == false ? 'button inline-flex items-center border-0 py-1 px-3 focus:outline-none text-base mt-4 md:mt-0' : 'button inline-flex items-center border-0 py-1 px-3 focus:outline-none rounded text-base mt-4 md:mt-0 btn-select'"
+          >
+            Open All Issues
+          </button>
+        </nav>
+        <nav class="md:ml-11 flex flex-wrap items-center text-base justify-center">
+          <button
+            @click="selectIssuesByRepo"
+            v-bind:class="this.selectRepoIssue == false ? 'button inline-flex items-center border-0 py-1 px-3 focus:outline-none text-base mt-4 md:mt-0' : 'button inline-flex items-center border-0 py-1 px-3 focus:outline-none rounded text-base mt-4 md:mt-0 btn-select'"
+          >
+            Issues By Repo
+          </button>
+        </nav>
+        <nav class="md:ml-11 flex flex-wrap items-center text-base justify-center">
+          <button
+            @click="selectIssueReport"
+            v-bind:class="this.selectReportIssue == false ? 'button inline-flex items-center border-0 py-1 px-3 focus:outline-none text-base mt-4 md:mt-0' : 'button inline-flex items-center border-0 py-1 px-3 focus:outline-none rounded text-base mt-4 md:mt-0 btn-select'"
+          >
+            Issue Report
+          </button>
+        </nav>
+      </div>
+    </header>
+  </template>
+  <script>
+  export default {
+    data() {
+      return {
+        selectAllIssues: false,
+        selectRepoIssue: false,
+        selectReportIssue: false,
+      };
+    },
+    methods: {
+      selectAllOpenIssues() {
+        this.selectAllIssues = true;
+        this.selectRepoIssue = false;
+        this.selectReportIssue = false;
+      },
+      selectIssuesByRepo() {
+        this.selectRepoIssue = true;
+        this.selectAllIssues = false;
+        this.selectReportIssue = false;
+      },
+      selectIssueReport() {
+        this.selectReportIssue = true;
+        this.selectAllIssues = false;
+        this.selectRepoIssue = false;
+      },
+    },
+  };
+  </script>
+  <style>
+  * {
+    font-family: "Montserrat";
+  }
+  .my-header {
+    background-color: #495175;
+  }
+  button {
+    color: white;
+  }
+  .btn-select {
+    color: black;
+    background-color: #F7B696;
+    border-radius: 20px;
+  }
+  </style>
--- src/views/GitHub.vue
+++ src/views/GitHub.vue
@@ -1,55 +1,15 @@
 <template>
-  <div class="home-view">
-    <div class="home-view__header">
-      <h3 class="home-view__title">Gantt Chart</h3>
-      <div class="home-view__form">
-        <input
-          type="text"
-          placeholder="username"
-          v-model="username"
-          @keyup.enter="fetchIssues"
-        />
-        <input
-          type="password"
-          placeholder="password"
-          v-model="password"
-          @keyup.enter="fetchIssues"
-        />
-      </div>
-    </div>
-  </div>
-  <FortyHours
-    v-if="username && password"
-    :username="username"
-    :password="password"
-  />
+  <FortyHours/>
 </template>
 
 <script>
 import FortyHours from "../components/FortyHours.vue";
-
 export default {
   name: "HomeView",
-  components: { FortyHours },
+  components: { FortyHours, },
   data() {
     return {
-      username: "",
-      password: "",
       fetch: false,
-      assignees: [
-        "MKRNaqeebi",
-        "usama2762",
-        "1abdurRehman",
-        "ahsan-coaldev",
-        "aishatasaduq",
-        "AliHanifdev1",
-        "ammarhaider001",
-        "hamza-coaldev",
-        "LaiqaRafiq",
-        "NajamBilalDev",
-        "noumandev11",
-        "RehmanCoaldev",
-      ],
     };
   },
   methods: {
