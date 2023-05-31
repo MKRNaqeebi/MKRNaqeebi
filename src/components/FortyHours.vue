156dba54227ab3baa2c6bb97e00f8be094cdd348
bob <bob@>
2023-05-31 13:40:14 +0000

    remove extra space

--- src/components/FortyHours.vue
+++ src/components/FortyHours.vue
@@ -95,7 +95,7 @@
           </div>
           <div class="conatiner bg-white drop-shadow-lg my-5 rounded-2xl h-screen " v-if="buttonClicked">
             <div class="gantt-div ">
-              <GanttChart v-if="username && password && repositories"  
+               <GanttChart v-if="username && password && repositories" 
                 :username="username" 
                 :password="password"
                 :repositories="repositories" />
